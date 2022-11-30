50ce132a10b17311cc7f60eb58c0496bd621bafe
theo-lemoine-oc <propjhh>
2022-11-30 03:42:42 +0100

    Initial commit

---
+++ frontend/nuxt.config.js
@@ -0,0 +1,79 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'DotStudio',
+    htmlAttrs: {
+      lang: 'fr'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Site de DotStudio' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    '@/assets/css/main.css',
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    '@nuxt/postcss8',
+    '@nuxtjs/google-fonts'
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    '@nuxtjs/pwa',
+    '@nuxtjs/fontawesome',
+  ],
+
+  fontawesome: {
+    'icons': {
+        'solid': true,
+        'brands': true
+    }
+  },
+
+  googleFonts: {
+    display: 'swap',
+    download: true,
+    overwriting: false,
+    inject: true,
+    families: {
+        Nunito: {
+            wght: [400, 600, 700, 900]
+        },
+    }
+  },
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    baseURL: '/',
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    postcss: {
+      plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+      },
+    },
+  }
+}
