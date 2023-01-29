060dc04cf50de08ef633be3fcdbfc582a7bec030
theo-lemoine-oc <propjhh>
2023-01-29 23:16:38 +0100

    fix router warn

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -45,7 +45,7 @@ export default {
   router: {
     extendRoutes(routes, resolve) {
       routes.push({
-        name: '404',
+        name: 'error',
         path: '*',
         component: resolve(__dirname, 'pages/404.vue')
       })
